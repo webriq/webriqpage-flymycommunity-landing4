8d440b7c33b48e828be538c28d4f851ad87d8d38
Eleazar Junsan <eljunz@gmail.com>
2018-09-26 18:47:07 +0800

    Updated gallery image 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,33 +16,6 @@ $(function() {
 $(document).ready(function(){
 
   TweenMax.from('#slider-content span', 1.25, {opacity: "0", y: -20} )
-  TweenMax.from('#slider-content h1', 2, {x:30, opacity: "0", delay: 1.5})
-
-  var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      spaceBetween: 20,
-      // Responsive breakpoints
-      breakpoints: {
-        // when window width is <= 320px
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        // when window width is <= 480px
-        480: {
-          slidesPerView: 1,
-          spaceBetween: 20
-        },
-        // when window width is <= 640px
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 30
-        }
-      }
-  });
+  TweenMax.from('#slider-content h1', 2, {x:30, opacity: "0", delay: 1.5})  
 
 });
